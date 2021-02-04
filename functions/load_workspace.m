e642534889b166ba7e9ad2fcadd80c56c0133d6b
darrahts <timothy.s.darrah@vanderbilt.edu>
2021-02-03 22:52:00 -0600

    fixed random number generator issue with parallel execution

--- functions/load_workspace.m
+++ functions/load_workspace.m
@@ -1,10 +1,10 @@
 function load_workspace(i, r_var, q_var, m_var, s_rate)
-    evalin('base', 'rng("shuffle")');
     evalin('base', 'seed = randi(9999)');
+    evalin('base', 'rng(seed)');
     evalin('base', 'load parallel_workspace.mat');
     evalin('base', sprintf('octomodel.sampletime = %f', s_rate));
-    evalin('base', sprintf('batterytwin.R0=abs(normrnd(rdeg(%d), %f))', i, r_var));
-    evalin('base', sprintf('batterytwin.Q=abs(normrnd(qdeg(%d), %f))', i, q_var));
+    evalin('base', sprintf('batterytwin.R0=max(abs(normrnd(rdeg(%d), %f)), .0001)', i, r_var));
+    evalin('base', sprintf('batterytwin.Q=min(abs(normrnd(qdeg(%d), %f)), 15.5)', i, q_var));
     evalin('base', sprintf('Motortwin2.Req=abs(normrnd(mdeg(%d), %f))', i, m_var));
     
 end
