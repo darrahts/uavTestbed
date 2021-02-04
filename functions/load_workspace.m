b72dc6b80aea101db85f9e914ad6e814ec543b47
darrahts <timothy.s.darrah@vanderbilt.edu>
2021-02-03 21:52:56 -0600

    added parameters for anonymous parallel function

--- functions/load_workspace.m
+++ functions/load_workspace.m
@@ -1,12 +1,11 @@
-function load_workspace(i, r_var, q_var, m_var)
+function load_workspace(i, r_var, q_var, m_var, s_rate)
     evalin('base', 'rng("shuffle")');
     evalin('base', 'seed = randi(9999)');
     evalin('base', 'load parallel_workspace.mat');
-    evalin('base', sprintf('batterytwin.R0=normrnd(rdeg(i), %f)', r_var));
-    evalin('base', sprintf('batterytwin.Q=normrnd(qdeg(i), %f)', q_var));
-    evalin('base', sprintf('Motortwin2.Req=normrnd(mdeg(i), %f)', m_var));
-%     evalin('base', sprintf('Motortwin4.Req=normrnd(mdeg(1), %f)', m_var));
-%     evalin('base', sprintf('Motortwin5.Req=normrnd(mdeg(1), %f)', m_var));
+    evalin('base', sprintf('octomodel.sampletime = %f', s_rate));
+    evalin('base', sprintf('batterytwin.R0=abs(normrnd(rdeg(%d), %f))', i, r_var));
+    evalin('base', sprintf('batterytwin.Q=abs(normrnd(qdeg(%d), %f))', i, q_var));
+    evalin('base', sprintf('Motortwin2.Req=abs(normrnd(mdeg(%d), %f))', i, m_var));
     
 end
 
