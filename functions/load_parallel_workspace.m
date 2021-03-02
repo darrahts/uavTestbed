dd687ff771babc4a16e1e2bf05192330cc71f243
darrahts <timothy.s.darrah@vanderbilt.edu>
2021-03-02 16:33:38 -0600

    commented out polyfit until its fixed

--- functions/load_parallel_workspace.m
+++ functions/load_parallel_workspace.m
@@ -1,4 +1,4 @@
-function load_parallel_workspace(i, lookback, horizon, r_deg, m_deg, q_deg, r_var, q_var, m_var, s_rate)
+function load_parallel_workspace(i, lookback, horizon, r_var, q_var, m_var, s_rate)
     evalin('base', 'seed = randi(9999)');
     evalin('base', 'rng(seed)');
     evalin('base', 'addpath(genpath(pwd))');
@@ -6,25 +6,25 @@ function load_parallel_workspace(i, lookback, horizon, r_deg, m_deg, q_deg, r_va
     evalin('base', 'load_trajectory');
     evalin('base', sprintf('octomodel.sampletime = %f', s_rate));
         
-    if i > lookback
-        % fit a polonomial to determine the rate of change of the degradation parameters 
-        evalin('base', sprintf('x = ((%d-(%d-1)):1:%d)', i, lookback, i));
-        evalin('base', sprintf("r_poly = polyfit(x, smoothdata(r_deg((%d-(%d-1)):%d)', 'rlowess', 5), 1)", i, lookback, i));
-        evalin('base', sprintf("q_poly = polyfit(x, smoothdata(q_deg((%d-(%d-1)):%d)', 'rlowess', 5), 1)", i, lookback, i));
-        evalin('base', sprintf("m_poly = polyfit(x, smoothdata(m_deg((%d-(%d-1)):%d)', 'rlowess', 5), 1)", i, lookback, i));
-        % keep track of deltas 
-        evalin('base', "polys(1,:) = r_poly");
-        evalin('base', "polys(2,:) = q_poly");
-        evalin('base', "polys(3,:) = m_poly");
-        
-        % predict degradation value at the horizon
-        evalin('base', sprintf("batterytwin.R0 = polyval(r_poly, %d+%d)", i, horizon));
-        evalin('base', sprintf("batterytwin.Q = polyval(q_poly, %d+%d)", i, horizon));
-        evalin('base', sprintf("Motortwin2.Req = polyval(m_poly, %d+%d)", i, horizon));
-        
-        evalin('base', sprintf('save_deg_info(%d, polys, R0i, Qi, RMi)', i));
-        
-    else
+%     if i > lookback
+%         % fit a polonomial to determine the rate of change of the degradation parameters 
+%         evalin('base', sprintf('x = ((%d-(%d-1)):1:%d)', i, lookback, i));
+%         evalin('base', sprintf("r_poly = polyfit(x, smoothdata(r_deg((%d-(%d-1)):%d)', 'rlowess', 5), 1)", i, lookback, i));
+%         evalin('base', sprintf("q_poly = polyfit(x, smoothdata(q_deg((%d-(%d-1)):%d)', 'rlowess', 5), 1)", i, lookback, i));
+%         evalin('base', sprintf("m_poly = polyfit(x, smoothdata(m_deg((%d-(%d-1)):%d)', 'rlowess', 5), 1)", i, lookback, i));
+%         % keep track of deltas 
+%         evalin('base', "polys(1,:) = r_poly");
+%         evalin('base', "polys(2,:) = q_poly");
+%         evalin('base', "polys(3,:) = m_poly");
+%         
+%         % predict degradation value at the horizon
+%         evalin('base', sprintf("batterytwin.R0 = polyval(r_poly, %d+%d)", i, horizon));
+%         evalin('base', sprintf("batterytwin.Q = polyval(q_poly, %d+%d)", i, horizon));
+%         evalin('base', sprintf("Motortwin2.Req = polyval(m_poly, %d+%d)", i, horizon));
+%         
+%         evalin('base', sprintf('save_deg_info(%d, polys, R0i, Qi, RMi)', i));
+%         
+%     else
         evalin('base', sprintf('batterytwin.R0 = max(abs(normrnd(rdeg(%d), %f)), .0001)', i, r_var));
         evalin('base', sprintf('batterytwin.Q = min(abs(normrnd(qdeg(%d), %f)), 15.5)', i, q_var));
         evalin('base', sprintf('Motortwin2.Req = abs(normrnd(mdeg(%d), %f))', i, m_var));
