3458c567b99d7cd247c893c8bfb01c9b545831e6
darrahts <timothy.s.darrah@vanderbilt.edu>
2021-03-04 14:06:23 -0600

    [WIP] schema for tables

--- sql/table_schema.sql
+++ sql/table_schema.sql
@@ -2,22 +2,22 @@
 
 create extension if not exists timescaledb;
 
-select create_hypertable('table_name', 'dt');
 
 create table uav_tb(
 	id serial primary key,
 	name varchar(32),
+	serial_number varchar(16) unique,
 	ct float not null default .0000085486,
 	cq float not null default .00000013678,
 	mass float not null default 1.8,
-	Jb float[9] not null default '{0.0429, 0.0, 0.0,    0.0, 0.0429, 0.0,   0.0, 0.0, 0.0748}',
-	Jbinv float[9] not null default '{23.31, 0.0, 0.0,  0.0, 23.31, 0.0,    0.0, 0.0, 13.369}',
+	"Jb" float[9] not null default '{0.0429, 0.0, 0.0,    0.0, 0.0429, 0.0,   0.0, 0.0, 0.0748}',
+	"Jbinv" float[9] not null default '{23.31, 0.0, 0.0,  0.0, 23.31, 0.0,    0.0, 0.0, 13.369}',
 	g float not null default 9.8,
 	sampletime float not null default 0.025,
 	cd float not null default 1.0, 
-	Axy float not null default 0.04,
-	Axz float not null default 0.04,
-	Ayz float not null default 0.04,
+	"Axy" float not null default 0.04,
+	"Axz" float not null default 0.04,
+	"Ayz" float not null default 0.04,
 	rho float not null default 1.2,
 	lx float not null default 0.2,
 	ly float not null default 0.2,
@@ -25,12 +25,11 @@ create table uav_tb(
 	l float not null default .45
 );
 
-create table component_tb(
-	id serial primary key,
-	name varchar(32) default 'base_component',
-	age_hours float default 0.0,
-	age_missions integer default 0, 
-);
+-- drop table uav_tb cascade;
+
+insert into uav_tb(name, serial_number) values('simulink_octocopter', 'X001');
+
+
 
 create table eqc_battery_tb(
 	id serial primary key,
@@ -39,17 +38,17 @@ create table eqc_battery_tb(
 	age_hours float default 0.0,
 	age_cycles integer default 0,
 	status varchar(32) default 'GREEN',
-	Q float not null default 15.0,
-	G float not null default 163.4413,
-	M float not null default 0.0092,
-	M0 float not null default 0.0019,
-	RC float not null default 3.6572,
-	R float not null default .00028283,
-	R0 float not null default -.0011,
+	"Q" float not null default 15.0,
+	"G" float not null default 163.4413,
+	"M" float not null default 0.0092,
+	"M0" float not null default 0.0019,
+	"RC" float not null default 3.6572,
+	"R" float not null default .00028283,
+	"R0" float not null default -.0011,
 	n float not null default 0.9987,
-	EOD float not null default 3.04,
+	"EOD" float not null default 3.04,
 	z float not null default 1.0,
-	Ir float not null default 0.0,
+	"Ir" float not null default 0.0,
 	h float not null default 0.0,
 	v float not null default 4.2,
 	v0 float not null default 4.2,
@@ -57,29 +56,35 @@ create table eqc_battery_tb(
 	uav_id integer references uav_tb(id)
 );
 
+-- drop table eqc_battery_tb ;
 
-create table battery_sensor_tb(
-	dt timestamptz default now(),
-	
-);
+insert into eqc_battery_tb(name, serial_number, uav_id) values ('plett_discrete', 'B001', 1);
 
-
-create table motor_tb(
-
-);
+-- select * from eqc_battery_tb where name ilike 'plett_discrete' and serial_number ilike 'B001' limit 1;
 
 
-
-create table sensor_reading_tb(
-	dt timestamptz default now(),
-);
-
-
-
-create table twin_sim_params(
-	
-
-);
+--create table motor_tb(
+--
+--);
+--
+--
+--
+--create table sensor_reading_tb(
+--	dt timestamptz default now(),
+--);
+--
+--
+--
+--create table twin_sim_params(
+--	
+--
+--);
+--
+--
+--create table battery_sensor_tb(
+--	dt timestamptz default now(),
+--	
+--);
 
 
 
