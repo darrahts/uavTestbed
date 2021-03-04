5311cdeea443e76bfc4b7072f2598bb089da0fd1
darrahts <timothy.s.darrah@vanderbilt.edu>
2021-03-04 13:06:18 -0600

    [WIP] updated uav_tb and eqc_battery_tb

--- sql/table_schema.sql
+++ sql/table_schema.sql
@@ -1,27 +1,38 @@
 
 
+create extension if not exists timescaledb;
+
+select create_hypertable('table_name', 'dt');
 
 create table uav_tb(
 	id serial primary key,
 	name varchar(32),
-
+	ct float not null default .0000085486,
+	cq float not null default .00000013678,
+	mass float not null default 1.8,
+	Jb float[9] not null default '{0.0429, 0.0, 0.0,    0.0, 0.0429, 0.0,   0.0, 0.0, 0.0748}',
+	Jbinv float[9] not null default '{23.31, 0.0, 0.0,  0.0, 23.31, 0.0,    0.0, 0.0, 13.369}',
+	g float not null default 9.8,
+	sampletime float not null default 0.025,
+	cd float not null default 1.0, 
+	Axy float not null default 0.04,
+	Axz float not null default 0.04,
+	Ayz float not null default 0.04,
+	rho float not null default 1.2,
+	lx float not null default 0.2,
+	ly float not null default 0.2,
+	lz float not null default 0.2,
+	l float not null default .45
 );
-
 
 create table component_tb(
 	id serial primary key,
-	name varchar(32) default 'default_uav',
+	name varchar(32) default 'base_component',
 	age_hours float default 0.0,
 	age_missions integer default 0, 
-
 );
 
-
-create table battery_chemistry_tb(
-
-);
-
-create table battery_circuit_tb(
+create table eqc_battery_tb(
 	id serial primary key,
 	name varchar(32) default 'default_battery',
 	serial_number varchar(16) unique,
@@ -47,15 +58,20 @@ create table battery_circuit_tb(
 );
 
 
-create table motor_tb(
+create table battery_sensor_tb(
+	dt timestamptz default now(),
+	
+);
 
+
+create table motor_tb(
 
 );
 
 
 
 create table sensor_reading_tb(
-
+	dt timestamptz default now(),
 );
 
 
@@ -63,5 +79,7 @@ create table sensor_reading_tb(
 create table twin_sim_params(
 	
 
-
 );
+
+
+
