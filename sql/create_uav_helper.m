01014d71043ffad450d0e38c89664fa76300fd5f
darrahts <timothy.s.darrah@vanderbilt.edu>
2021-06-26 12:58:23 -0500

    added comments

--- sql/create_uav_helper.m
+++ sql/create_uav_helper.m
@@ -1,7 +1,21 @@
-function serial_number = create_uav_helper(conn, owner, ser_idx)
-
+function serial_number = create_uav_helper(conn, owner, serial_number)
+    %%
+    %       @brief: helper function to create a UAV
+    %
+    %       @params: 
+    %           conn - the database connection object
+    %           owner - the username of the owner
+    %           serial_number - the unique identifier of the UAV
+    %
+    %       @returns: the serial number with 'u' appended to the front for
+    %       UAV. Then the user can call load_uav(conn, serial_number)
+    %
+    %       @constraints: 
+    %           ~exists(serial_number)
+    %
+    %%
     type_id       = 1; % airframe
-    serial_number = sprintf("x%06d", ser_idx);
+    serial_number = sprintf("x%06d", serial_number);
     num_motors    = 8;
     age           = 0;
     units         = "flight hours";
@@ -18,7 +32,7 @@ function serial_number = create_uav_helper(conn, owner, ser_idx)
 
     type_id       = 2; % motor   
     for i=1:airframe.num_motors
-        serial_number = sprintf("m%06d-%d", ser_idx, i);
+        serial_number = sprintf("m%06d-%d", serial_number, i);
         motor = create_default_motor(conn, ...
                                      owner, ...
                                      type_id, ...
@@ -29,7 +43,7 @@ function serial_number = create_uav_helper(conn, owner, ser_idx)
     end                               
                                
     type_id       = 3; % battery 
-    serial_number = sprintf("b%06d", ser_idx);
+    serial_number = sprintf("b%06d", serial_number);
     units         = "cumulative amp hours";
     battery = create_default_battery(conn, ...
                                      owner, ...
@@ -40,7 +54,7 @@ function serial_number = create_uav_helper(conn, owner, ser_idx)
     
     type_id       = 4; % uav
     units         = "flight hours";
-    serial_number = sprintf("u%06d", ser_idx);
+    serial_number = sprintf("u%06d", serial_number);
     uav = create_default_uav(conn, ...
                          owner, ...
                          type_id, ...
