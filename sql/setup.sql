bfa969823c8b873ee8ab64fa9c90a5d7d7d83ea2
darrahts <timothy.s.darrah@vanderbilt.edu>
2021-07-10 11:11:26 -0500

    update testing

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,9 +1,10 @@
 
 -- create the database
-create database uav_db;
+-- create database uav_db;
 
--- create a database user 
-create user johndoe with encrypted password 'P@$$word1';
+-- -- create a database user 
+-- create user johndoe with encrypted password 'P@$$word1';
 
--- grant permissions
-grant all privileges on database uav_db to johndoe;
+-- -- grant permissions
+-- grant all privileges on database uav_db to johndoe;
+raise notice 'value: %', &1;
