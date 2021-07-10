cfa7a2f566a26e7419f38147b7fa5fa9bbe412ba
darrahts <timothy.s.darrah@vanderbilt.edu>
2021-07-10 10:50:30 -0500

    adding sql setup file

---
+++ sql/setup.sql
@@ -0,0 +1,9 @@
+
+-- create the database
+create database uav_db;
+
+-- create a database user 
+create user johndoe with encrypted password 'P@$$word1';
+
+-- grant permissions
+grant all privileges on database uav_db to johndoe;
