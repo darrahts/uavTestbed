25f0b2bc606f2cd450e9e7089af14b9b12c7a548
darrahts <timothy.s.darrah@vanderbilt.edu>
2021-07-11 11:14:07 -0500

    added read only guest user

--- sql/setup_db_user.sql
+++ sql/setup_db_user.sql
@@ -16,10 +16,18 @@
 -- create the database
 create database uav_db;
 
--- create a database user 
+-- create a database user (you)
 create user :user with encrypted password :passwd;
 
--- grant permissions
+-- grant permissions to you
 grant all privileges on database uav_db to :user;
 
+-- give yourself admin access
 alter user :user with superuser;
+
+-- create a read-only guest account 
+create user guest with encrypted password 'P@$$word1';
+grant connect on database uav_db to guest;
+grant usage on schema public to guest;
+grant select on all tables in schema public to guest;
+alter user guest with connection limit 10;
