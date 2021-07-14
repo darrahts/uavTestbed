e0fa3dd0530fb9b8bd009af665ec2e28bdfd7c6e
darrahts <timothy.s.darrah@vanderbilt.edu>
2021-07-14 15:18:23 -0500

    fixed

--- sql/get_column_names.m
+++ sql/get_column_names.m
@@ -1,7 +1,19 @@
 function cols = get_column_names(conn, table_name)
-
+    %%
+    %       @brief: get all column names of a given table as a list of
+    %       strings
+    %
+    %       @params: 
+    %           conn - the database connection object
+    %           table_name - the table to get the columns of
+    %
+    %       @returns: a list of column names as strings
+    %
+    %       @constraints: 
+    %           exists(table_name)
+    %%
     cols = table2array(select(conn, sprintf("select array(select ''''||column_name||'''' column_name FROM information_schema.columns WHERE table_schema = 'public' AND table_name = '%s')", table_name)));
-    cols = [string(cols{1}).replace('{', '').replace('}', '').replace('''', '').split(',')];
-
+    cols = [string(cols{1}).replace('{', '').replace('}', '').replace('''', '').split(',')]';
+    %cols = cellstr(cols);
 end
 
