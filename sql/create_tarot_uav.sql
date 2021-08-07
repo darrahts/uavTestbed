cef7c73819303e27f0fdbb0f9123773eec60f1a3
darrahts <timothy.s.darrah@vanderbilt.edu>
2021-08-06 22:00:49 -0500

    updated parameter value

--- sql/create_tarot_uav.sql
+++ sql/create_tarot_uav.sql
@@ -31,7 +31,7 @@ do $$
 	begin
 		insert into default_airframe_tb ("id", "num_motors", "ct", "cq", "mass", "Jb", "cd", "Axy", "Axz", "Ayz", "l")
 								 values (airframe_id, num_motors, .000098419, .0000018503, 10.66, '{0.2506,0.0, 0.0,     0.0, 0.2506, 0.0,   0.0, 0.0, 0.4538}', 1.0, 1.6129, 0.508, 0.508, .635);
-		insert into eqc_battery_tb ("id", "Q", "EOD", "v", "v0") values (battery_id, 16.0, 15.54, 22.2, 22.2);
+		insert into eqc_battery_tb ("id", "Q", "EOD", "v", "v0") values (battery_id, 11, 17.54, 22.2, 22.2);
 		insert into dc_motor_tb ("id", "motor_number", "Req", "Ke_eq", "J")
 			values (motor_ids[1], 1, .27, .0265, .00005),
 				(motor_ids[2], 2, .27, .0265, .00005),
@@ -51,7 +51,8 @@ do $$
 				"m5_id",
 				"m6_id",
 				"m7_id",
-				"m8_id")
+				"m8_id", 
+				"max_flight_time")
 			values (uav_id,
 				airframe_id,
 				battery_id,
@@ -62,5 +63,6 @@ do $$
 				motor_ids[5],
 				motor_ids[6],
 				motor_ids[7],
-				motor_ids[8]);	  
+				motor_ids[8],
+				30);	  
 end $$;
