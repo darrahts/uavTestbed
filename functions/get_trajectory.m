8dbfa4a8d9d069079575fbc602edd93106d1f649
darrahts <timothy.s.darrah@vanderbilt.edu>
2021-08-16 19:22:27 -0500

    refactored

--- functions/get_trajectory.m
+++ functions/get_trajectory.m
@@ -1,4 +1,4 @@
-function trajectory = get_trajectory(file_name, max_flight_time, desired_velocity)
+function trajectory = get_trajectory(trajectory_tb, id)
 %%
 %
 %       TODO: function comments, refactor into the api folder. update.
@@ -9,72 +9,48 @@ function trajectory = get_trajectory(file_name, max_flight_time, desired_velocit
 %
 %%
     
-    trajectory_tb = readtable(file_name);
-    
-    trajectory_tb = trajectory_tb(trajectory_tb.path_time < max_flight_time, :);
-    trajectory_tb = sortrows(trajectory_tb, "path_time", 'descend');
-
-    if height(trajectory_tb) < 1
-        disp("[INFO] returning empty table")
-        trajectory = table();
-        return;
-    else
-        idx = randi(height(trajectory_tb));
-        trajectory = table2struct(trajectory_tb(idx,:));
-    end
-    
+    trajectory = table2struct(trajectory_tb(1,:));
     trajectory.x_waypoints = str2double(regexp(trajectory.x_waypoints,'[+-]?\d+\.?\d*','match'));
     trajectory.y_waypoints = str2double(regexp(trajectory.y_waypoints,'[+-]?\d+\.?\d*','match'));
     trajectory.waypoints = [trajectory.x_waypoints' trajectory.y_waypoints'];
+    trajectory.start = str2double(regexp(trajectory.start,'[+-]?\d+\.?\d*','match'));
+    trajectory.start = [trajectory.start];
+
+    trajectory.destination = str2double(regexp(trajectory.destination,'[+-]?\d+\.?\d*','match'));
+    trajectory.destination = [trajectory.destination];
     
-    if isempty(trajectory.x_ref_points)
-        map = load('trajectories/test_map.mat');
-        map = map.test_map;
-        Resolution=0.1; % meters
-        map = binaryOccupancyMap(map,Resolution);
-        uavRadius = .5;
-        start = [50 50];    
-        % inflate map
-        inflate(map,uavRadius);
+    
+    if sum(strcmp(fieldnames(trajectory), 'path')) == 0
+        map = load(sprintf('trajectories/%s.mat', trajectory.map)).(sprintf('%s', trajectory.map));
         prm = mobileRobotPRM;
         prm.Map = map;
+        prm.NumNodes = 2000;
+        prm.ConnectionDistance = 50;
 
-        prm.NumNodes = 5000;
-        prm.ConnectionDistance = 100;
-        waypoints = [start];
-        waypoints2 = [trajectory.x_waypoints' trajectory.y_waypoints'];
-        waypoints = [waypoints; waypoints2];
-        path = findpath(prm, start, waypoints(1,:));
-        for i = 2:length(waypoints(:,1))
-           p = findpath(prm, waypoints(i-1,:), waypoints(i,:));
-           path = [path; p];
+        trajectory.path = findpath(prm, trajectory.start, trajectory.waypoints(1,:));
+        for i = 2:length(trajectory.waypoints(:,1))
+           p = findpath(prm, trajectory.waypoints(i-1,:), trajectory.waypoints(i,:));
+           trajectory.path = [trajectory.path; p];
         end
+    end
 
-        path_distance = calculatedistance(path); % total distance to be covered
-        time_interval = calculatetime(path_distance,desired_velocity); 
-        % generate smooth time stamped trajectory
-        sampletimetraj=.25; % 0.5 seconds by default
-        tSamples = sampletimetraj:sampletimetraj:time_interval(2);
-        [q,qd,qdd,pp] = bsplinepolytraj(path',time_interval,tSamples);
-        x_ref_points = [tSamples',q(1,:)'];
-        y_ref_points = [tSamples',q(2,:)'];
-        trajectory.x_ref_points = x_ref_points(:,2);
-        trajectory.y_ref_points = y_ref_points(:,2);
-        
+    if isempty(trajectory.x_ref_points)
+        trajectory.reference_velocity = 1.3;        
+        trajectory.path_distance = calculatedistance(trajectory.path); % total distance to be covered
+        time_interval = calculatetime(trajectory.path_distance,trajectory.reference_velocity); 
+        trajectory.path_time = time_interval(2)/60;
+        time_samples = 1:trajectory.sample_time:time_interval(2);
+        [q, qd, qdd, pp] = bsplinepolytraj(trajectory.path', time_interval, time_samples);
+
+        trajectory.x_ref_points = [time_samples' q(1,:)'];
+        trajectory.y_ref_points = [time_samples' q(2,:)'];
     else
+        time_samples = [1:trajectory.sample_time:trajectory.path_time*60]';
         trajectory.x_ref_points = str2double(regexp(trajectory.x_ref_points,'[+-]?\d+\.?\d*','match'))';
         trajectory.y_ref_points = str2double(regexp(trajectory.y_ref_points,'[+-]?\d+\.?\d*','match'))';
-        sampletimetraj=1;
+        trajectory.x_ref_points = [time_samples trajectory.x_ref_points]
     end
-    
     trajectory = rmfield(trajectory, {'x_waypoints', 'y_waypoints'});
-    times = [1:1:length(trajectory.x_ref_points)]'*sampletimetraj;
-    trajectory.x_ref_points = [times trajectory.x_ref_points];
-    trajectory.y_ref_points = [times trajectory.y_ref_points];
-    
-    % these two parameters should match with what was used when the
-    % trajectories were generated. 
-%    trajectory.sample_rate = 1.0; % s
-%    trajectory.velocity = 1.3; % m/s
+
 end
 
